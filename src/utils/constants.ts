be312f600d8b86cdf80295e1b916d57f56b6bcfc
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-30 12:31:55 +0800

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-// const proxy = "https://proxy.techzbots1.workers.dev/?u="
+// const proxy = "https://animedexapi.rexcalumba86.workers.dev/?u="
 const animedexServers: string[] = import.meta.env.VITE_API_SERVERS.split(' ')
 
 
